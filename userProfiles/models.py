5c869f7936b9956ccf0652bd19849cdd19d50e0a
Clark Bristol <clarkbristol@CJ-Bristols-MacBook-Pro.local>
2016-05-25 21:25:15 -0400

    Second Commit

--- userProfiles/models.py
+++ userProfiles/models.py
@@ -4,7 +4,10 @@ Source: https://docs.djangoproject.com/en/1.8/topics/auth/customizing/#extending
 """
 from django.db import models
 from django.contrib.auth.models import User
+from claims.models import Claim
 
 class StandardUser(models.Model):
     user = models.OneToOneField(User)
     authority = models.IntegerField(default=0)
+    #ADD LIST OF AFFIRMED CLAIMS https://docs.djangoproject.com/en/1.9/ref/models/fields/#model-field-types
+    affirms = models.ManyToManyField(Claim) 
