6504d8c78fb14073dc68750d14d9fa342a6bddf5
Clark Bristol <clarkbristol@CJ-Bristols-MacBook-Pro.local>
2016-06-25 19:07:28 -0400

    added comments w/ links to sources AND updated requirements file for Heroku

--- userProfiles/__init__.py
+++ userProfiles/__init__.py
@@ -1,1 +1,4 @@
+# what are signals? appConfig? https://chriskief.com/2014/02/28/django-1-7-signals-appconfig/
+
+
 default_app_config = 'userProfiles.apps.userProfilesConfig'
--- userProfiles/models.py
+++ userProfiles/models.py
@@ -1,6 +1,9 @@
 """
 Extend the existing User model (which only deals with authentication etc)
-Source: https://docs.djangoproject.com/en/1.8/topics/auth/customizing/#extending-the-existing-user-model
+Source:
+	https://docs.djangoproject.com/en/1.8/topics/auth/customizing/#extending-the-existing-user-model
+Solved problem of user not getting created (expands on mysterious last sentence of above link):
+	http://stackoverflow.com/questions/28748281/extending-user-profile-in-django-1-7
 """
 from django.db import models
 from django.contrib.auth.models import User
